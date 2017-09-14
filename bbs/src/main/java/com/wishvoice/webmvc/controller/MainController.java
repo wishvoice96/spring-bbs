8f93d7831e7cc1bfea2c2807c31ff04288dae940
wishvoice <wishvoice@wishvoice-PC>
2017-09-14 21:52:13 +0900

    logback 설정

--- bbs/src/main/java/com/wishvoice/webmvc/controller/MainController.java
+++ bbs/src/main/java/com/wishvoice/webmvc/controller/MainController.java
@@ -1,13 +1,19 @@
 package com.wishvoice.webmvc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class MainController {
 
+	private   Logger logger =LoggerFactory.getLogger(MainController.class);
+	
 		@RequestMapping("/")
 		public String main() {
+			logger.debug("spring logback main");
 			return "main";
 		}
 }
